478b01891cfedb1895fa00e2a88b83d2b4ee72e8
vava24680 <9203700@gmail.com>
2017-05-25 12:53:42 +0800

    Add Pipe_Reg_Write_i port for hold the value in Pipe_Reg.

--- code/Pipe_Reg.v
+++ code/Pipe_Reg.v
@@ -2,31 +2,40 @@
 //--------------------------------------------------------------------------------
 //Version:     1
 //--------------------------------------------------------------------------------
-//Writer:      
+//Writer:
 //----------------------------------------------
-//Date:        
+//Date:
 //----------------------------------------------
-//Description: 
+//Description:
 //--------------------------------------------------------------------------------
 module Pipe_Reg(
     clk_i,
 	rst_i,
 	data_i,
+	Pipe_Reg_Write_i,
 	data_o
 	);
 
 parameter size = 0;
 
-input					clk_i;		  
+input					clk_i;
 input					rst_i;
 input		[size-1: 0]	data_i;
+input Pipe_Reg_Write_i;
 output reg	[size-1: 0]	data_o;
 
 always @(posedge clk_i) begin
     if(~rst_i)
         data_o <= 0;
     else
-        data_o <= data_i;
+		if(Pipe_Reg_Write_i)
+			begin
+				data_o <= data_i;
+			end
+		else
+			begin
+				data_o <= data_o;
+			end
 end
 
-endmodule	
+endmodule
